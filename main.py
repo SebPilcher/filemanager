58deb205aacfc40301484c6e9b56332b6803cfa5
Seb <s.t.pilcher@student.saintkentigern.com>
2022-07-29 09:07:48 +1200

    fourth commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from genericpath import isdir
 import os
+from send2trash import send2trash
+from pickle import TRUE
 import sys
 from pathlib import Path
-from PyQt5.QtWidgets import QApplication, QWidget, QTreeView, QFileSystemModel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QTreeView, QFileSystemModel, QHBoxLayout
 from PyQt5.QtCore import QModelIndex
+from PyQt5.QtGui import QKeySequence
 
 class FileSystemView(QWidget):
     def __init__(self, path_str):
@@ -25,16 +29,60 @@ class FileSystemView(QWidget):
         self.tree.expand(self.model.index(path.as_posix()))
         self.tree.expandAll()
 
-
+        self.tree.selectionModel().currentChanged.connect(self.currentChanged)
+        self.tree.doubleClicked.connect(self.doubleClicked)
 
         self.tree.setColumnWidth(0, 300)
 
+        self.preview = PreviewPanel()
 
-        layout = QVBoxLayout()
+
+
+        layout = QHBoxLayout()
         layout.addWidget(self.tree)
+        layout.addWidget(self.preview)
 
 
         self.setLayout(layout)
+
+    def currentChanged(self, index):
+        if not self.model.isDir(index):
+            self.preview.displayPreview(self.model.filePath(index))
+            
+        
+
+    def doubleClicked(self, signal):
+        index = self.tree.currentIndex()
+
+        if not self.model.isDir(index):
+            os.startfile(self.model.filePath(index))
+
+    def keyPressEvent(self, event):
+        index = self.tree.currentIndex()
+
+
+        if event == QKeySequence.StandardKey.InsertParagraphSeparator:
+            if self.model.isDir(index):
+                self.tree.expand(index)
+            else:
+                os.startfile(self.model.filePath(index))
+        if event == QKeySequence.StandardKey.Delete:
+            file_path = self.model.filePath(index)
+            file_path = file_path.replace("/","\\")
+            send2trash(file_path)
+
+        
+
+
+class PreviewPanel(QWidget):
+    def __init__(self):
+        super().__init__()
+
+
+    def displayPreview(self, path):
+        # display preview of path
+        pass
+
 
 
 
